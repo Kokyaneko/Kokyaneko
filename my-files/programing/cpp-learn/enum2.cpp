5f7f28d98cc54f86711915cbac5da6e9dd8838ad
Ryo Arima <r.arima.1359@gmail.com>
2026-08-19 02:05:18 +0900

    Add enum2.cpp
    Add enum2.cpp to learn enum.(Values are chenged from enum1.c)

---
+++ my-files/programing/cpp-learn/enum2.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+int main (void){
+    enum alpha {exit, x, y=3, z, invalid};
+    /*
+    exit=0
+    x=1
+    y=3
+    z=4
+    invalid=5
+    */
+
+    int a;
+    do{
+        cout<<"0:exit 1:a 2:b 3:c >";
+        cin >> a;
+    } while(a<exit or a>=invalid);
+
+    alpha b = static_cast<alpha>(a);//int型変数aをalpha型に変換する
+
+    switch(b){
+        case 0 : return 0;
+        case 1 : cout << 'A' << endl; break;//breakしないとcase2,3も実行される
+        case 3 : cout << 'B' << endl; break;//breakしないとcase3も実行される
+        case 4 : cout << 'C' << endl; break;
+    }
+
+    return 0;
+}
