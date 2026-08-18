d05da7bcb33e396efeff9f7ebeed2ea38beadcf3
Ryo Arima <r.arima.1359@gmail.com>
2026-08-18 23:24:30 +0900

    Add github-Aauth system
    Add github-Aauth system.
    This program is used for github auth instead of gh command.
    Please set .github-Aauth.txt in your home directry.

---
+++ my-files/programing/github-Aauth/github-Aauth.cpp
@@ -0,0 +1,38 @@
+/*
+This program is used for github auth instead of gh command.
+Please set .github-Aauth.txt in your home directry.
+
+This program is written by Arima Ryo, 2026.
+This program is perfectly free.
+*/
+
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+
+using namespace std;
+
+int main(void){
+    const char *HOME_DIR_PATH = getenv("HOME");//std::getenv <-Get environment variable. 
+
+    string PERFECT_PATH = string(HOME_DIR_PATH) + "/.github-Aauth.txt";
+
+    FILE *fp = fopen(PERFECT_PATH.c_str() ,"r");
+    if(fp == NULL){
+        cout<<"Not found:"<<PERFECT_PATH<<endl;
+        return 1;
+    }
+
+    char trushable[72];
+    char uname[72];
+    char pass[72];
+
+    fscanf(fp,"%s",trushable);
+    fscanf(fp,"%s %s",trushable,uname);
+    fscanf(fp,"%s %s",trushable,pass);
+
+    cout<<uname<<endl;
+    cout<<pass<<endl;
+
+    return 0;
+}
