308bc1cf45cc1a72b352448d25c9c9b611c3b73d
Ryo Arima <r.arima.1359@gmail.com>
2026-08-20 00:27:02 +0900

    Add bool cpp code
    Add bool cpp code to learn bool.

---
+++ my-files/programing/cpp-learn/bool2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main (void){
+    int a;
+
+    cout<<"Enter 3 :";
+    cin>>a;
+
+    cout<<boolalpha;
+    cout<<"T/F a=3 : "<< (a == 3) <<endl;
+
+    return 0;
+}
