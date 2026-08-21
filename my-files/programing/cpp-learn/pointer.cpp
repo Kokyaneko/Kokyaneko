fbeb964aeea8c71dd039ebe52bf61f8ac8985294
Ryo Arima <r.arima.1359@gmail.com>
2026-08-22 02:23:13 +0900

    Add pointer.cpp

---
+++ my-files/programing/cpp-learn/pointer.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void){
+    int a;
+
+    cout<<"enter number: ";
+    cin >> a;
+    int &p = a;//Cとは異なり、ポインタは&nのように宣言できる。また、アドレスではなく、変数名を直接代入できる。
+
+    cout<<"a: "<< a <<endl;
+    cout<<"&p(=a): "<< p <<endl;
+
+    p = 3;
+    cout<<"a(changed from p): "<< p <<endl;
+
+    return 0;
+}
