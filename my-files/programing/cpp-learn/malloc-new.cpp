83e8dc0c8f820b1f8fe70fe4307b76c046c6e9b0
Ryo Arima <r.arima.1359@gmail.com>
2026-08-24 00:22:12 +0900

    Edit malloc-new.cpp

--- my-files/programing/cpp-learn/malloc-new.cpp
+++ my-files/programing/cpp-learn/malloc-new.cpp
@@ -4,17 +4,18 @@
 
 int main (void){
     int a = 100;
+    std::cout<<"a(int): "<< a <<std::endl;
 
     //malloc - C
     int *p = (int*)malloc(sizeof(int));//C++では型判定が厳格なため、int*型に変更する。
     *p = a;
-    printf("*p(malloc): %d %p\n",*p,p);
+    printf("*p=100(malloc): %d %p\n",*p,p);
     free(p);
 
     //new - C++
     int *pp = new int;
     *pp = 200;
-    std::cout<<"*pp(new): "<<*pp<<' '<<pp<<std::endl;
+    std::cout<<"*pp=200(new): "<< *pp <<' '<< pp <<std::endl;
     delete p;
 
     return 0;
